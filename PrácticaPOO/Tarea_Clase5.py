62c107db48ba1d535fabec1698c4bdaff8bbfede
karmaly <karol.alvarez9@gmail.com>
2024-03-03 16:50:21 -0500

    Arreglando la busqueda por partes

--- PrácticaPOO/Tarea_Clase5.py
+++ PrácticaPOO/Tarea_Clase5.py
@@ -1,3 +1,4 @@
+import re
 # Creación de objeto Paciente
 class Paciente:
     def __init__(self):
@@ -42,12 +43,12 @@ class Sistema:
                 break
         return encontrado
  
-    def verDatosPaciente(self,cedula):
-        if self.verificarPac(cedula) == False:
-            return None
-        for p in self.__lista_pacientes:
-            if cedula == p.verCedula():
-                return p
+    #def verDatosPaciente(self,cedula):
+    #    if self.verificarPac(cedula) == False:
+    #        return None
+    #    for p in self.__lista_pacientes:
+    #        if cedula == p.verCedula():
+    #            return p
                        
     def ingresarPaciente(self,pac):
         if self.verificarPac(pac.verCedula()):
@@ -78,7 +79,7 @@ def validarNumero(numero):
 def main():
     sis = Sistema()
     while True:
-        opcionv = input("\nIngrese para:\n0. Para volver al menu \n1. Ingresar nuevo paciente \n2. Ver paciente \n3. Ver cantidad de pacientes \n>> ")
+        opcionv = input("\nIngrese para:\n0. Volver al menu \n1. Ingresar nuevo paciente \n2. Ver paciente \n3. Ver cantidad de pacientes \n>> ")
         a = validarNumero(opcionv)
         if a:
             opcion = int(opcionv)
@@ -112,28 +113,53 @@ def main():
                     print("Paciente ya existe en el sistema")
 
             elif opcion == 2:
-                # 1. Solicito la cedula que quiero buscar
+                # 1. Solicito el nombre o cedula que quiero buscar
                 while True:
-                    cedulav = input("Ingrese la cedula a buscar: ")
-                    c = validarNumero(cedula)
-                    if c:
-                        cedula = int(cedulav)
-                        break
-                    else:
-                        print("Debe ser un dato numérico (sin puntos ni letras)")
-                        continue
-                # Le pido al sistema que me devuelva en la variable p al paciente que tenga
-                # la cedula en la lista
-                p = sis.verDatosPaciente(cedula)
-                # Si encunetro el paciente imprimo los datos
-                if p == None:
-                    print("El paciente no se encotró")
-                else:
-                    print("Nombre: " + p.verNombre())
-                    print("Cedula: " + str(p.verCedula()))
-                    print("Genero: " + p.verGenero())
-                    print("Servicio: " + p.verServicio())
-            
+                    busquedav: input("\nIngrese para:\n0. Busqueda por cédula o parte de ella \n1. Nombre o parte de él\n>> ")
+                    a = validarNumero(busquedav)
+                    if a:
+                        busqueda = int(busquedav)
+                        if busqueda == 0:
+                            while True:
+                                cedulav = input("Ingrese la cedula o parte de ella: ")
+                                c = validarNumero(cedula)
+                                if c:
+                                    cedula = int(cedulav)
+                                    break
+                                else:
+                                    print("Debe ser un dato numérico (sin puntos ni letras)")
+                                    continue
+                        # Le pido al sistema que me devuelva en la variable p al paciente que tenga
+                        # la cedula en la lista
+                            patron = re.complie(f".*{cedula}.*")
+                            c = 0
+                            for cedula in sis:
+                                if patron.match(cedula.verCedula()):
+                                    # Si encunetro el paciente imprimo los datos
+                                    c += 1
+                                    print("Nombre: " + p.verNombre())
+                                    print("Cedula: " + str(p.verCedula()))
+                                    print("Genero: " + p.verGenero())
+                                    print("Servicio: " + p.verServicio())
+                                else: 
+                                    print("No se tiene coincidencias.")
+                        if busqueda == 1:
+                            nombre = input("Ingrese el nombre o parte de él: ")
+                        # Le pido al sistema que me devuelva en la variable p al paciente que tenga
+                        # la cedula en la lista
+                        patron = re.complie(f".*{cedula}.*")
+                        for cedula in sis:
+                            if patron.match(cedula.verCedula()):
+                                # Si encunetro el paciente imprimo los datos
+                                print("Los pacientes que tienen valores similares a la cédula buscada son:")
+                                print("Nombre: " + p.verNombre())
+                                print("Cedula: " + str(p.verCedula()))
+                                print("Genero: " + p.verGenero())
+                                print("Servicio: " + p.verServicio())
+                            else: 
+                                print("No se tiene coincidencias.")
+                    else: 
+                        print("Ingrese una opción válida")
             elif opcion == 3:
                 print(f"La cantidad de pacientes en el sistema es: {sis.verNumeroPacientes()}")
                         
