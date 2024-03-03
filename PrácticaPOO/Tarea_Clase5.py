33eae6f65c1de8f58225d824dde6d23371e262e5
karmaly <karol.alvarez9@gmail.com>
2024-03-03 17:36:42 -0500

    Revisión por partes

--- PrácticaPOO/Tarea_Clase5.py
+++ PrácticaPOO/Tarea_Clase5.py
@@ -35,6 +35,9 @@ class Sistema:
         self.__lista_pacientes = []
 
 # Funciones del objeto paciente 
+    def verLista(self):
+        return self.__lista_pacientes
+    
     def verificarPac(self,cedula):
         encontrado = False
         for p in self.__lista_pacientes:
@@ -42,7 +45,7 @@ class Sistema:
                 encontrado = True
                 break
         return encontrado
-                       
+      
     def ingresarPaciente(self,pac):
         if self.verificarPac(pac.verCedula()):
             return False
@@ -106,14 +109,15 @@ def main():
                     print("Paciente ya existe en el sistema")
 
             elif opcion == 2:
-                # 1. Solicito el nombre o cedula que quiero buscar
                 while True:
-                    busquedav: input("\nIngrese para:\n0. Busqueda por cédula o parte de ella \n1. Nombre o parte de él\n>> ")
+                    # 1. Solicito el nombre o cedula que quiero buscar
+                    busquedav = input("\nIngrese para:\n0. Busqueda por cédula o parte de ella \n1. Nombre o parte de él\n>> ")
                     a = validarNumero(busquedav)
                     if a:
                         busqueda = int(busquedav)
                         if busqueda == 0:
                             while True:
+                                # Valido que sea un dato numérico
                                 cedulav = input("Ingrese la cedula o parte de ella: ")
                                 c = validarNumero(cedula)
                                 if c:
@@ -122,13 +126,12 @@ def main():
                                 else:
                                     print("Debe ser un dato numérico (sin puntos ni letras)")
                                     continue
-                        # Le pido al sistema que me devuelva en la variable p al paciente que tenga
-                        # la cedula en la lista
+                            # Tomo el valor ingresado como un 
                             patron = re.complie(f".*{cedula}.*")
                             c = 0
-                            for cedula in sis:
-                                if patron.match(cedula.verCedula()):
-                                    # Si encunetro el paciente imprimo los datos
+                            listadispobible = sis.verLista()
+                            for p in listadispobible:
+                                if patron.match(p.verCedula()):
                                     c += 1
                                     print("Nombre: " + p.verNombre())
                                     print("Cedula: " + str(p.verCedula()))
@@ -138,18 +141,18 @@ def main():
                                 print("No se tiene coincidencias.")
                         if busqueda == 1:
                             nombre = input("Ingrese el nombre o parte de él: ")
-                        # Le pido al sistema que me devuelva en la variable p al paciente que tenga
-                        # la cedula en la lista
-                        patron = re.complie(f".*{cedula}.*")
-                        for cedula in sis:
-                            if patron.match(cedula.verCedula()):
-                                # Si encunetro el paciente imprimo los datos
-                                print("Los pacientes que tienen valores similares a la cédula buscada son:")
-                                print("Nombre: " + p.verNombre())
-                                print("Cedula: " + str(p.verCedula()))
-                                print("Genero: " + p.verGenero())
-                                print("Servicio: " + p.verServicio())
-                            else: 
+
+                        patron = re.complie(f".*{nombre}.*")
+                        c = 0
+                            listadispobible = sis.verLista()
+                            for p in listadispobible:
+                                if patron.match(p.verNombre()):
+                                    c += 1
+                                    print("Nombre: " + p.verNombre())
+                                    print("Cedula: " + str(p.verCedula()))
+                                    print("Genero: " + p.verGenero())
+                                    print("Servicio: " + p.verServicio())
+                            if c == 0: 
                                 print("No se tiene coincidencias.")
                     else: 
                         print("Ingrese una opción válida")
