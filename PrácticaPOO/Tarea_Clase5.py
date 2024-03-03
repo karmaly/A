b35e6e00c364fe24562a8c47170d52352ad87250
karmaly <karol.alvarez9@gmail.com>
2024-03-03 18:34:36 -0500

    FIN. Espero esté bueno

--- PrácticaPOO/Tarea_Clase5.py
+++ PrácticaPOO/Tarea_Clase5.py
@@ -1,5 +1,6 @@
+# Importo una función que me permite buscar patrones dentro de cadenas.
 import re
-# Creación de objeto Paciente
+# Creación de la clase Paciente
 class Paciente:
     def __init__(self):
         # Atributos
@@ -8,7 +9,7 @@ class Paciente:
         self.__genero = ""
         self.__servicio = ""
 
-# Getters del objeto Paciente
+# Getters de la clase Paciente
     def verNombre(self):
         return self.__nombre  
     def verServicio(self):
@@ -18,7 +19,7 @@ class Paciente:
     def verCedula(self):
         return self.__cedula
     
-# Setters del objeto Paciente
+# Setters de la clase Paciente
     def asignarNombre(self,n):
         self.__nombre = n   
     def asignarServicio(self,s):
@@ -28,13 +29,13 @@ class Paciente:
     def asignarCedula(self,c):
         self.__cedula = c
 
-# Creación de objeto Sistema
+# Creación de la clase Sistema
 class Sistema:
     def __init__(self):
         # Atributos
         self.__lista_pacientes = []
 
-# Funciones del objeto paciente 
+# Funciones de la clase Sistema
     def verLista(self):
         return self.__lista_pacientes
     
@@ -63,7 +64,8 @@ class Sistema:
         
     def verNumeroPacientes(self):
         return len(self.__lista_pacientes)
-    
+
+# Funcion para validar entero
 def validarNumero(numero):
     try:
         a = int(numero)
@@ -73,8 +75,10 @@ def validarNumero(numero):
     
 # Creación de función main para iniciar el código:    
 def main():
+    # Creo el objeto Sistema
     sis = Sistema()
     while True:
+        # Brindo opciones de lo que se desea hacer en un menú y valido la escogida sea válida.
         opcionv = input("\nIngrese para:\n0. Volver al menu \n1. Ingresar nuevo paciente \n2. Ver paciente \n3. Ver cantidad de pacientes \n>> ")
         a = validarNumero(opcionv)
         if a:
@@ -84,6 +88,7 @@ def main():
                 # 1. Se solicitaran los datos
                 nombre = input("Ingrese el nombre: ")
                 while True:
+                    # Valido si el dato ingresado para la cédula corresponde a un entero
                     cedulav = input("Ingrese la cedula: ")
                     c = validarNumero(cedulav)
                     if c:
@@ -96,13 +101,13 @@ def main():
                 servicio = input("Ingrese el servicio: ")
                 # 2. Se crea un objeto Paciente
                 pac = Paciente()
-                # Como es paciente esta vacio debo ingresarle la informacion
+                # Como ese paciente esta vacio debo ingresarle la informacion
                 pac.asignarNombre(nombre)
                 pac.asignarCedula(cedula)
                 pac.asignarGenero(genero)
                 pac.asignarServicio(servicio)
                 r = sis.ingresarPaciente(pac)
-                # 3 Se almacena en la lista que esta dentro de la clase sistema
+                # 3. Se almacena en la lista que esta dentro de la clase sistema
                 if r == True:
                     print("Paciente ingresado")
                 else:
@@ -110,8 +115,8 @@ def main():
 
             elif opcion == 2:
                 while True:
-                    # 1. Solicito el nombre o cedula que quiero buscar
-                    busquedav = input("\nIngrese para:\n0. Busqueda por cédula o parte de ella \n1. Nombre o parte de él\n>> ")
+                    # Solicito el nombre o cedula que quiero buscar
+                    busquedav = input("\nIngrese para:\n0. Busqueda por cédula o parte de ella \n1. Nombre o parte de él\n2. Volver al menú principal\n>> ")
                     a = validarNumero(busquedav)
                     if a:
                         busqueda = int(busquedav)
@@ -125,11 +130,16 @@ def main():
                                 else:
                                     print("Debe ser un dato numérico (sin puntos ni letras)")
                                     continue
-                            # Tomo el valor ingresado como un 
+                            # Tomo el valor ingresado como un patrón mediante la función re.compile() para realizar 
+                            # operaciones de búsqueda y coincidencia de patrones de manera más eficiente. También creo un contador
+                            # que servirá para saber si se obtubieron o no resultados en la búsqueda.
                             patron = re.compile(f".*{cedulav}.*")
                             c = 0
+                            # Accedo mediante una visualización el atributo del objeto lista. 
                             listadispobible = sis.verLista()
                             for p in listadispobible:
+                            # Modifico a "str" el atributo relacionado con la cédula dado que el método match requiere que argumento sea "str".
+                            # En caso de hallar alguna coincidencia se imprimen todos los atributos del objeto "Paciente".
                                 if patron.match(str(p.verCedula())):
                                     c += 1
                                     print("Nombre: " + p.verNombre())
@@ -138,21 +148,24 @@ def main():
                                     print("Servicio: " + p.verServicio())
                             if c == 0: 
                                 print("No se tiene coincidencias.")
+                        # Se hace el mismo análisis de la anterior busqueda, pero al ser "str" no requiero validar que sea
+                        # un dato numérico. Pero procedo con el método re.compile() de igual manera.
                         if busqueda == 1:
                             nombre = input("Ingrese el nombre o parte de él: ")
-
-                        patron = re.compile(f".*{nombre}.*")
-                        c = 0
-                        listadispobible = sis.verLista()
-                        for p in listadispobible:
-                            if patron.match(p.verNombre()):
-                                c += 1
-                                print("Nombre: " + p.verNombre())
-                                print("Cedula: " + str(p.verCedula()))
-                                print("Genero: " + p.verGenero())
-                                print("Servicio: " + p.verServicio())
-                        if c == 0: 
-                            print("No se tiene coincidencias.")
+                            patron = re.compile(f".*{nombre}.*")
+                            c = 0
+                            listadispobible = sis.verLista()
+                            for p in listadispobible:
+                                if patron.match(p.verNombre()):
+                                    c += 1
+                                    print("Nombre: " + p.verNombre())
+                                    print("Cedula: " + str(p.verCedula()))
+                                    print("Genero: " + p.verGenero())
+                                    print("Servicio: " + p.verServicio())
+                            if c == 0: 
+                                print("No se tiene coincidencias.")
+                        if busqueda == 2:
+                            break
                     else: 
                         print("Ingrese una opción válida")
             elif opcion == 3:
