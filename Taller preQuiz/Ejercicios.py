a0e1e15ef27dc9990ba4e3af8ac3bcdf3e94f4a5
karmaly <karol.alvarez9@gmail.com>
2024-04-11 14:46:44 -0500

    Creación de la copia de la primera matríz

--- Taller preQuiz/Ejercicios.py
+++ Taller preQuiz/Ejercicios.py
@@ -6,3 +6,8 @@ import scipy.io as sio
 # 1. matríz aleatoria 4D de size 12000000
 m4D = np.random.rand(20,20,300,100)
 print(m4D.size)
+
+#2. Copia de la matríz anterior pero con 3D
+copiam4D = m4D[:,:,:,0].copy()
+print(copiam4D.shape)
+print(m4D.shape)
