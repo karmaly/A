7b434d2a1aa589aa6a5c845c727ad885e5b4bc9f
karmaly <karol.alvarez9@gmail.com>
2024-04-11 14:36:19 -0500

    Creación de la matríz del punto 1

---
+++ Taller preQuiz/Ejercicios.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib as plt
+import pandas as pt
+import scipy.io as sio
+
+# 1. matríz aleatoria 4D de size 12000000
+m4D = np.random.rand(20,20,300,100)
+print(m4D.size)
