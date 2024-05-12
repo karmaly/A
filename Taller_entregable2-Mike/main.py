7c0ac31ca7997bca258445df766db0026fd1fb34
karmaly <karol.alvarez9@gmail.com>
2024-05-12 16:00:33 -0500

    Unificación

--- Taller_entregable2-Mike/main.py
+++ Taller_entregable2-Mike/main.py
@@ -4,31 +4,11 @@ import cv2
 import numpy as np 
 import os
 
-def grafica(ima1, ima2):
-    plt.figure(figsize=(15,6))
-    plt.subplot(1,2,1)
-    plt.imshow(ima1, cmap='inferno', vmin=0, vmax=255)
-    plt.subplot(1,2,2)
-    plt.imshow(ima2, cmap='inferno', vmin=0, vmax=255)
-    plt.show()
 
-print('Primer punto\n')
-imagen = cv2.imread('Imagen_celula.jpg')
-img_gray = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY) #matriz
-#binarización 
-umb , imagen_binaria = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY) #devuleve imagen_binaria como una matriz
-print(np.shape(imagen_binaria)) 
-#gradiente para mejor presicion 
-kernel = np.ones((5,5),np.uint8)
-imagen_gradiente=cv2.morphologyEx(imagen_binaria, cv2.MORPH_GRADIENT, kernel, iterations = 1)
-grafica(imagen_binaria, imagen_gradiente)
-#etiquetado
-elem,mask=cv2.connectedComponents(imagen_gradiente)
-print(f'Número de células: {elem}\n')
 
 print('\npunto 2\n')
 
-data = 'archivosDCM/'
+data = 'Entregable2/archivosDCM'
 
 archivos_dcm = [archivo for archivo in os.listdir(data) if archivo.endswith('.dcm')]
 
